6736a7bceca7860537f8a8425c11652fcd2c1b73
truonglx <truonglx@truonglxs-iMac.local>
2019-03-06 15:36:13 +0700

    cắt ngắn chữ khi quá dài

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 import React, {Component} from 'react';
-import {View, Text, TouchableHighlight, Image, TouchableOpacity, ScrollView, Animated, Easing, StyleSheet} from 'react-native';
+import {View, Text,Dimensions, TouchableHighlight, Image, TouchableOpacity, ScrollView, Animated, Easing, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+
+var rong = (Dimensions.get('screen').width/2-150)/2;
 
 class DropdownMenu extends Component {
 
@@ -36,6 +38,8 @@ class DropdownMenu extends Component {
       return (
         <View style={{flex: 1, justifyContent: 'space-between', alignItems: "center", paddingHorizontal: 15, flexDirection: 'row'}} >
     <Text
+    numberOfLines={1}
+    ellipsizeMode="tail"
       style={[
         styles.item_text_style,
         this.props.optionTextStyle,
@@ -51,7 +55,10 @@ class DropdownMenu extends Component {
     } else {
       return (
         <View style={{flex: 1, justifyContent: 'space-between', alignItems: "center", paddingHorizontal: 15, flexDirection: 'row'}} >
-    <Text style={[
+    <Text 
+    numberOfLines={1}
+    ellipsizeMode="tail"
+    style={[
         styles.item_text_style,
         this.props.optionTextStyle,
         {color: this.props.tintColor ? this.props.tintColor : this.defaultConfig.tintColor}
@@ -72,7 +79,7 @@ class DropdownMenu extends Component {
       }
 
       return (
-        <View style={{width:150,marginTop:10,marginLeft:10,position: 'absolute', left: 0, right: 0, top: 40, bottom: 0}}>
+        <View style={{marginLeft:rong,width:150,position: 'absolute',marginTop: -8, left: 0, right: 0, top: 40, bottom: 0}}>
     <TouchableOpacity onPress={() => this.openOrClosePanel(this.state.activityIndex)} activeOpacity={1} style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}}>
     <View style={{opacity: 0.4, backgroundColor: 'white', flex: 1 }} />
     </TouchableOpacity>
@@ -188,6 +195,7 @@ class DropdownMenu extends Component {
   <View style={{
       flexDirection: 'row', borderWidth:1,
       borderRadius:4,
+      padding:10,
       borderColor:"#b1b1b1",
       width:150,
       // margin:10,
@@ -198,9 +206,11 @@ class DropdownMenu extends Component {
       activeOpacity={1}
       onPress={this.openOrClosePanel.bind(this, index)}
       key={index}
-      style={{flex: 1, height: 30, alignItems: "center", justifyContent: "center"}} >
+      style={{flex: 1, height: 20, alignItems: "center", justifyContent: "center"}} >
     <View style={{flexDirection: 'row', alignItems: "center", justifyContent: "center"}} >
     <Text
+    numberOfLines={1}
+    ellipsizeMode="tail"
       style={[
         styles.title_style,
         this.props.titleStyle,
@@ -242,7 +252,9 @@ DropdownMenu.propTypes = {
 
 const styles = StyleSheet.create({
   title_style: {
-    fontSize: 14
+    paddingLeft: 5,
+    paddingRight: 5,
+    fontSize: 14,
   },
   item_text_style: {
     color: '#333333',
